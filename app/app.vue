38e61640bb0a35efe9e0a0767a33f6843ccc9f37
Saurlax <saurlax@qq.com>
2025-10-07 16:59:12 +0800

    feat: remove login page

--- app/app.vue
+++ app/app.vue
@@ -4,19 +4,53 @@ useHead({
 });
 
 const { loggedIn } = useUserSession();
-const route = useRoute();
+import type { AuthFormField } from "@nuxt/ui";
 
-watchEffect(() => {
-  if (!loggedIn.value && route.path !== "/login") {
-    navigateTo("/login");
-  }
-});
+const { fetch } = useUserSession();
+const toast = useToast();
+const fields: AuthFormField[] = [
+  {
+    name: "username",
+    type: "text",
+    label: "用户名",
+    placeholder: "请输入用户名",
+    required: true,
+  },
+  {
+    name: "password",
+    type: "password",
+    label: "密码",
+    placeholder: "请输入密码",
+    required: true,
+  },
+];
+
+function login(payload: any) {
+  $fetch("/api/login", {
+    method: "POST",
+    body: payload.data,
+  })
+    .then(async () => {
+      await fetch();
+      navigateTo("/");
+    })
+    .catch((err) => {
+      toast.add({
+        title: err.message,
+        description: err.data.message,
+        color: "warning",
+      });
+    });
+}
 </script>
 
 <template>
   <UApp :toaster="{ position: 'top-center' }">
     <NuxtLayout>
-      <NuxtPage />
+      <NuxtPage v-if="loggedIn" />
+      <UPageCard v-else class="max-w-sm mx-auto mt-4">
+        <UAuthForm title="Login" :fields="fields" @submit.prevent="login" />
+      </UPageCard>
     </NuxtLayout>
   </UApp>
 </template>
