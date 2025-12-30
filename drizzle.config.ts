f483e732cef2431025071e0a3c38fe7cece845dc
Saurlax <saurlax@qq.com>
2025-12-31 02:14:12 +0800

    feat: update drizzle schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   out: "./drizzle",
-  schema: "./server/utils/drizzle.ts",
+  schema: "./server/utils/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
